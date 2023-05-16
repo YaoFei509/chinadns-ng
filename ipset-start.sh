d78e53dfc22d777598740754e01315a0481bd25a
姚 飞 <feiyao@me.com>
2023-05-16 15:48:32 +0800

    add chnnip4/6 gfwip4/6

--- ipset-start.sh
+++ ipset-start.sh
@@ -2,5 +2,8 @@
 
 /usr/sbin/ipset -R -exist -f /usr/local/etc/chnroute.ipset
 /usr/sbin/ipset -R -exist -f /usr/local/etc/chnroute6.ipset
-
+/usr/sbin/ipset create chnip4 hash:net family inet
+/usr/sbin/ipset create chnip6 hash:net family inet6
+/usr/sbin/ipset create gfwip4 hash:net family inet
+/usr/sbin/ipset create gfwip6 hash:net family inet6
 exit 0
--- ipset-stop.sh
+++ ipset-stop.sh
@@ -2,5 +2,9 @@
 
 /usr/sbin/ipset -F chnroute
 /usr/sbin/ipset -F chnroute6
+/usr/sbin/ipset save chnip4 -f /home/yaofei/chnip4.ipset
+/usr/sbin/ipset save chnip6 -f /home/yaofei/chnip6.ipset
+/usr/sbin/ipset save gfwip4 -f /home/yaofei/gfwip4.ipset
+/usr/sbin/ipset save gfwip6 -f /home/yaofei/gfwip6.ipset
 
 exit 0
