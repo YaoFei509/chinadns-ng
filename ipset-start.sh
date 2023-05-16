10b07b8b5c7cf5daab99615d789e1149b0c9f57b
姚 飞 <feiyao@me.com>
2023-05-16 16:13:14 +0800

    save and restore chn/gfw ip4/6 ipset

--- ipset-start.sh
+++ ipset-start.sh
@@ -1,9 +1,11 @@
-#!/bin/sh -e
+#!/bin/bash
 
-/usr/sbin/ipset -R -exist -f /usr/local/etc/chnroute.ipset
-/usr/sbin/ipset -R -exist -f /usr/local/etc/chnroute6.ipset
-/usr/sbin/ipset create chnip4 hash:net family inet
-/usr/sbin/ipset create chnip6 hash:net family inet6
-/usr/sbin/ipset create gfwip4 hash:net family inet
-/usr/sbin/ipset create gfwip6 hash:net family inet6
+IPSET=/usr/sbin/ipset
+
+$IPSET -R -exist -f /usr/local/etc/chnroute.ipset
+$IPSET -R -exist -f /usr/local/etc/chnroute6.ipset
+[[ -f /usr/local/etc/chnip6.ipset ]] && $IPSET -R -exist -f /usr/local/etc/chnip6.ipset  || $IPSET create chnip6 hash:net family inet6
+[[ -f /usr/local/etc/chnip4.ipset ]] && $IPSET -R -exist -f /usr/local/etc/chnip4.ipset  || $IPSET create chnip4 hash:net family inet
+[[ -f /usr/local/etc/gfwip6.ipset ]] && $IPSET -R -exist -f /usr/local/etc/gfwip6.ipset  || $IPSET create gfwip6 hash:net family inet6
+[[ -f /usr/local/etc/gfwip4.ipset ]] && $IPSET -R -exist -f /usr/local/etc/gfwip4.ipset  || $IPSET create gfwip4 hash:net family inet
 exit 0
